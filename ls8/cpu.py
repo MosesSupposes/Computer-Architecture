42ab216765b1c10c84425928322a04c5f581cfd4
Moses A. Samuel <mosamuel98@gmail.com>
2020-05-18 15:52:17 -0700

    modify the constructor, ram_write, ram_read; implement the run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,16 +1,18 @@
 """CPU functionality."""
 
-import sys
-
 class CPU:
+    commands = {
+       "HLT": 0b01,
+       "LDI": 0b10000010,
+       "PRN": 0b01000111
+    }
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = []
-        self.reg = []
+        self.ram = [0] * 256 
+        self.reg = [0] * 8
         self.pc = 0
-        pass
 
     def load(self):
         """Load a program into memory."""
@@ -37,11 +39,11 @@ class CPU:
         try: 
             return self.ram[address]
         except IndexError:
-            print("The address of value  " + str(address) +  " isn't a valid location in memory")
-            sys.exit(1)
+            raise ValueError("The address of value  " + str(address) +  " isn't a valid location in memory")
+            
     
-    def ram_write(self, value):
-        self.ram.append(value)
+    def ram_write(self, address, value):
+        self.ram[address] = value
         return value
 
     def alu(self, op, reg_a, reg_b):
@@ -75,4 +77,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        IR = None
+        while IR != self.commands["HLT"]:
+            IR = self.ram_read(self.pc)
+
+            if IR == self.commands["HLT"]:
+                self.pc += 1
+                break
+
+            elif IR == self.commands["LDI"]:
+                self.reg[self.ram[self.pc + 1]] = self.ram[self.pc + 2]
+                self.pc += 3
+            
+            elif IR == self.commands["PRN"]:
+                print(self.reg[self.ram[self.pc + 1]])
+                self.pc += 2
+
+            else:
+                print(f"Invalid Command: {IR}")
+                break
+        
+        # Reset the program counter
+        self.pc = 0
